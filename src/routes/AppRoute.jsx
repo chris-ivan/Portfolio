a342ae416ea920ee6462631163e607feb08d8d4a
Christopher Ivan <gunardi.ivan@gmail.com>
2020-12-26 21:15:47 +0700

    add react-ga

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from "react";
 import { Route } from "react-router-dom";
 
-const AppRoute = ({ component: Component, path }) => {
+const AppRoute = ({ component: Component, path, history }) => {
   return (
     <Route
       path={path}
+      history={history}
       render={(props) => (
         <Suspense fallback={""}>
           <Component {...props} />
